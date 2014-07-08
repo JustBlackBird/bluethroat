c8c4dc1d76728010990389ca64641dd5f2028a8a
Dmitriy Simushev <simushevds@gmail.com>
2014-07-08 22:19:30 +0400

    Update "express" module

--- app.js
+++ app.js
@@ -18,30 +18,26 @@ var radio = new Radio(
         // Initialize entire application
         var app = express();
 
-        // Configure the application
-        app.configure(function() {
-            // Set default values for runtime configs
-            app.set('wake up hour', 9);
-            app.set('wake up minute', 45);
-            app.disable('alarm');
-
-            // Use Handlebars template engine
-            app.set('view engine', 'handlebars');
-            app.engine('handlebars', require('hbs').__express);
-
-            // Load and configure middleware
-            app.use(express.static(__dirname + '/public'));
-            app.use(express.urlencoded());
-            app.use(app.router);
-            app.use(middleware.notFound);
-            app.use(middleware.serverError);
-        });
-
         // Attache radio to the application object
         app.radio = radio;
 
+        // Set default values for runtime configs
+        app.set('wake up hour', 9);
+        app.set('wake up minute', 45);
+        app.disable('alarm');
+
+        // Use Handlebars template engine
+        app.set('view engine', 'handlebars');
+        app.engine('handlebars', require('hbs').__express);
+
+        // Load and configure middleware
+        app.use(express.static(__dirname + '/public'));
+
         // Initialize routes
         routes.init(app);
+
+        app.use(middleware.notFound);
+        app.use(middleware.serverError);
 
         // Initialize alarm clock
         alarm.init(app);
