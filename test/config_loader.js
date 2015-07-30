f770f5ad703a72c3b21b788f918eab3c26735315
Dmitriy Simushev <simushevds@gmail.com>
2015-07-30 11:03:31 +0300

    Use mock-fs to test config loader

--- test/config_loader.js
+++ test/config_loader.js
@@ -1,29 +1,44 @@
 var path = require('path'),
     should = require('should'),
+    mockFs = require('mock-fs'),
     ConfigLoader = require('../lib/config_loader');
-
-var getConfigPath = function(relativePath) {
-    return path.normalize(__dirname + '/fixtures/configs/' + relativePath);
-};
 
 describe('ConfigLoader', function() {
     describe('load', function() {
         it('should throw exception if the file does not exists', function() {
+            mockFs({
+                // There are no files at all.
+            });
+
             (function() {
-                ConfigLoader.load(getConfigPath('missed.json'));
+                ConfigLoader.load('configs/missed.json');
             }).should.throw(/^File \".*?missed\.json\" is not found/);
+
+            mockFs.restore();
         });
 
         it('should throw exception if not a file passed in', function() {
+            mockFs({
+                'configs/not-a-file': {/* empty directory */}
+            });
+
             (function() {
-                ConfigLoader.load(getConfigPath('not_a_file'));
+                ConfigLoader.load('configs/not-a-file');
             }).should.throw('Specified config file is not a regular file');
+
+            mockFs.restore();
         });
 
         it('should throw exception on invalid JSON', function() {
+            mockFs({
+                'configs/invalid.json': '{'
+            });
+
             (function() {
-                ConfigLoader.load(getConfigPath('invalid.json'));
+                ConfigLoader.load('configs/invalid.json');
             }).should.throw(/^Cannot parse config file/);
+
+            mockFs.restore();
         });
     });
 });
