f31cb9a050f96b5a018d9547c5f1bee2da963581
Dmitriy Simushev <simushevds@gmail.com>
2015-10-30 00:16:11 +0300

    Throw away "ready" event from Radio

--- app.js
+++ app.js
@@ -29,72 +29,70 @@ var radio = new Radio(mpdPool);
 
 radio.setCurrentStation(_.findWhere(config.radioStations, {id: config.defaultRadioStation}));
 
-radio.on('ready', function() {
-    // Initialize entire application
-    var app = express();
+// Initialize entire application
+var app = express();
 
-    // Initialize alarm clock
-    var alarm = new AlarmClock();
+// Initialize alarm clock
+var alarm = new AlarmClock();
 
-    // Actually the http server should be initialize AFTER all callbacks of
-    // "settings.get" method will be ran. This is not done in that way because
-    // of simplicity. In most cases it should not brings any problems because
-    // "settings.get" method is really fast and finishes before the HTTP server
-    // is up and running.
-    // TODO: Initialize HTTP server only when all "settings.get" callbacks will
-    // be ran.
+// Actually the http server should be initialize AFTER all callbacks of
+// "settings.get" method will be ran. This is not done in that way because
+// of simplicity. In most cases it should not brings any problems because
+// "settings.get" method is really fast and finishes before the HTTP server
+// is up and running.
+// TODO: Initialize HTTP server only when all "settings.get" callbacks will
+// be ran.
 
-    // Get the current alarm time from the storage.
-    settings.get('alarm_time', function(error, time) {
-        if (error) {
-            throw error;
-        }
+// Get the current alarm time from the storage.
+settings.get('alarm_time', function(error, time) {
+    if (error) {
+        throw error;
+    }
 
-        if (time) {
-            alarm.setTime(time.hours, time.minutes);
-        } else {
-            // There is no alarm time stored. Use a default one.
-            alarm.setTime(9, 45);
-        }
-    });
-
-    // Get the current state of alarm clock.
-    settings.get('alarm_enabled', function(error, isEnabled) {
-        if (error) {
-            throw error;
-        }
-
-        if (isEnabled) {
-            alarm.run();
-        }
-    });
-
-    alarm.on('ring', function() {
-        if (!radio.isPlaying()) {
-            // Time to wake up. Turn on the radio
-            radio.fadeIn();
-        }
-    });
-
-    // Use Handlebars template engine
-    app.set('view engine', 'handlebars');
-    app.engine('handlebars', require('hbs').__express);
-
-    // Serve static files
-    app.use(express.static(__dirname + '/public'));
-
-    // Mount routes
-    app.use('/', routes(radio, stationsKeeper, alarm, settings));
-
-    // Mount other middleware
-    app.use(middleware.notFound);
-    app.use(middleware.serverError);
-
-    // Initialize HTTP server
-    app.listen(
-        config.server.port,
-        function() {
-            console.log('Application is up on port ' + config.server.port.toString());
-        }
-    );
+    if (time) {
+        alarm.setTime(time.hours, time.minutes);
+    } else {
+        // There is no alarm time stored. Use a default one.
+        alarm.setTime(9, 45);
+    }
 });
+
+// Get the current state of alarm clock.
+settings.get('alarm_enabled', function(error, isEnabled) {
+    if (error) {
+        throw error;
+    }
+
+    if (isEnabled) {
+        alarm.run();
+    }
+});
+
+alarm.on('ring', function() {
+    if (!radio.isPlaying()) {
+        // Time to wake up. Turn on the radio
+        radio.fadeIn();
+    }
+});
+
+// Use Handlebars template engine
+app.set('view engine', 'handlebars');
+app.engine('handlebars', require('hbs').__express);
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+// Mount routes
+app.use('/', routes(radio, stationsKeeper, alarm, settings));
+
+// Mount other middleware
+app.use(middleware.notFound);
+app.use(middleware.serverError);
+
+// Initialize HTTP server
+app.listen(
+    config.server.port,
+    function() {
+        console.log('Application is up on port ' + config.server.port.toString());
+    }
+);
